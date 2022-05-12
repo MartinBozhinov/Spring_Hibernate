1d96e8856bf56f4373cfa93dfc7ba1bb2bcb69d4
Martin Bozhinov <sharkytrapera@gmail.com>
2022-05-12 07:23:12 +0300

    hibernate advanced mappings one_to_one

---
+++ Hibernate_advanced_mappings/src/main/java/com/example/onetooneuni/entity/Instructor.java
@@ -0,0 +1,79 @@
+package com.example.onetooneuni.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "instructors")
+public class Instructor {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private InstructorDetail instructorDetail;
+
+    public Instructor() {
+    }
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    public InstructorDetail getInstructorDetail() {
+        return instructorDetail;
+    }
+
+    public void setInstructorDetail(InstructorDetail instructorDetail) {
+        this.instructorDetail = instructorDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", instructorDetail=" + instructorDetail +
+                '}';
+    }
+}
---
+++ Hibernate_advanced_mappings/src/main/java/com/example/onetooneuni/entity/InstructorDetail.java
@@ -0,0 +1,51 @@
+package com.example.onetooneuni.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "instructor_details")
+public class InstructorDetail {
+
+    private Long id;
+    private String youtubeChannel;
+    private String hobby;
+
+    public InstructorDetail() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @Column(name = "youtube_channel")
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+    @Column(name = "hobby")
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
